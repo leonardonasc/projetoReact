8d00dff3e2fd07ba4e1d9aaf043fd3e030f24dac
Leonardo Nascimento <leonardo.private@hotmail.com>
2023-03-02 13:32:33 -0300

    feat: página de login e form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,17 +1,29 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Login = () => {
-    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleClick = () => {
-        navigate('./pagina-inicial')
+    const handleEntrar = () => {
+        console.log(email, password);
+
     }
 
-    return (
+return (
         <div>
-            Login
-
-            <button onClick={handleClick}>Página inicial</button>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+            </form>
         </div>
     );
 }
